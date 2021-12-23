038a31522582366a9755316db165ca7eb64c26de
phh95 <602646761@qq.com>
2021-12-23 21:53:02 +0800

    update GET_UP_MESSAGE_TEMPLATE

--- get_up.py
+++ get_up.py
@@ -7,7 +7,7 @@ from github import Github
 # 14 for test 12 real get up
 GET_UP_ISSUE_NUMBER = 1   
 GET_UP_MESSAGE_TEMPLATE = (
-    "今天的起床时间是--{get_up_time}.\r\n\r\n 起床啦，喝杯咖啡，背个单词，去跑步。\r\n\r\n 今天的一句诗:\r\n {sentence}"
+    "今天的起床时间是--{get_up_time}.\r\n\r\n 懒猪🐷起床啦，赶紧去跑步，上班不迟到。\r\n\r\n 今天的一句诗:\r\n {sentence}"
 )
 SENTENCE_API = "https://v1.jinrishici.com/all"
 DEFAULT_SENTENCE = "赏花归去马如飞\r\n去马如飞酒力微\r\n酒力微醒时已暮\r\n醒时已暮赏花归\r\n"
