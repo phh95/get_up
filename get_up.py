20948cb3a60dd167c24470a84d85c28e78f5682a
phh95 <602646761@qq.com>
2021-12-23 14:39:01 +0800

    Update get_up.py

--- get_up.py
+++ get_up.py
@@ -43,7 +43,7 @@ def make_get_up_message():
     sentence = get_one_sentence()
     now = pendulum.now(TIMEZONE)
     # 3 - 6 means early for me
-    is_get_up_early = 3 <= now.hour <= 6
+    is_get_up_early = 6 <= now.hour <= 18  
     get_up_time = now.to_datetime_string()
     body = GET_UP_MESSAGE_TEMPLATE.format(get_up_time=get_up_time, sentence=sentence)
     return body, is_get_up_early
